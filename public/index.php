c520abc27319e81d4def4ea64f8b3123ac6debe3
Gustaf Brodin <gustafbrodin@hotmail.com>
2021-01-12 15:10:37 +0100

    added geolocation with "show my location-button"

--- public/index.php
+++ public/index.php
@@ -76,57 +76,14 @@ try {
     lng = "<?=($officespecs['office_name'])?>";
     <?php } ?> 
 
+    <!-- <div id="mapholder"></div>
+      <form>
+         <input type="button" onclick="getLocation();" value="Get Location"/>
+      </form> -->
+
     
-  
-
-  <!-- <script> 
-  
     
-    var markers = [
-      {
-        coords:{lat:lat,lng:lng},
-        iconImage:'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
-        content:'<h4>Norrsken</h4>'
-      },
-      {
-        coords:{lat:42.8584,lng:-70.9300},
-        content:'<h1>Amesbury MA</h1>'
-      },
-      {
-        coords:{lat:42.7762,lng:-71.0773}
-      }
-    ];
 
-    for(var i = 0;i < markers.length;i++){
-      // Add marker
-      addMarker(markers[i]);
-    }
-
-    // Add Marker Function
-    function addMarker(props){
-      var marker = new google.maps.Marker({
-        position:props.coords,
-        map:map,
-        //icon:props.iconImage
-      });
-
-      // Check for customicon
-      if(props.iconImage){
-        // Set icon image
-        marker.setIcon(props.iconImage);
-      }
-
-      // Check content
-      if(props.content){
-        var infoWindow = new google.maps.InfoWindow({
-          content:props.content
-        });
-
-        marker.addListener('click', function(){
-          infoWindow.open(map, marker);
-        });
-      }
-    }
-    </script> -->
+  
   </body>
 </html>
